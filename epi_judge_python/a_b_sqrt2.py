dbb8045a3e7c44da598916fc975d0ceb4aa8f230
Gabriela <tborges.gabriela@gmail.com>
2018-05-29 18:32:27 +0000

    test

--- epi_judge_python/a_b_sqrt2.py
+++ epi_judge_python/a_b_sqrt2.py
@@ -5,7 +5,7 @@ def generate_first_k_a_b_sqrt2(k):
     # TODO - you fill in here.
     return []
 
-
+#test
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main("a_b_sqrt2.py", 'a_b_sqrt2.tsv',
